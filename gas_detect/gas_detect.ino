536302bfaa19be23d21ab5dd6b3dea4c4a7627fe
Kadir <kadirerturk@gmail.com>
2021-11-11 16:34:11 -0800

    interval 10sec

--- gas_detect/gas_detect.ino
+++ gas_detect/gas_detect.ino
@@ -8,14 +8,18 @@
 #include <PubSubClient.h>
 #include <Wire.h>
 #include "SparkFun_SGP30_Arduino_Library.h" 
+#include "MICS6814.h"
 
 SGP30 mySensor;
-
-#define NO2 27
-#define NH3 26
-#define CO  25
-
 #define I2C_SGP30  0x58
+
+#define PIN_NO2 27
+#define PIN_NH3 26
+#define PIN_CO  25
+MICS6814 gas(PIN_CO, PIN_NO2, PIN_NH3);
+
+
+
 /*TFT_WHITE TFT_BLACK TFT_YELLOW TFT_RED TFT_GREEN TFT_BLUE*/
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
@@ -26,30 +30,21 @@ const char* mqttServer = "";
 int mqttPort = 1883;
 
 long last_time = 0;
-char myData[100];
+char myData[255];
 
 
 WiFiClient wifiClient;
-
 PubSubClient mqttClient(wifiClient);
 
-/* Finds absolute humidity */
-uint32_t getAbsoluteHumidity(float temperature, float humidity) {
-  float absoluteHumidity = 213.7f * (humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature);
-  uint32_t scaledHumidity = (uint32_t) (1000.0f * absoluteHumidity); 
-  return scaledHumidity;
-}
 
+/* ########################### FUNCTIONS ######################*/
+/* Init the TFT object */
 void initTFT() {
   tft.init();
   tft.begin();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_YELLOW, TFT_BLACK); // Note: the new fonts do not draw the background colour
-  /* 
-   *  tft.setFont(1);
-   *  
-   */
 }
 
 /* Print text to TFT screen generic function */
@@ -61,17 +56,15 @@ void printTFT(int x, int y, int font, char * str) {
   tft.drawString(str, x, y);
 }
 
-
+/* Setup MQTT */
 void setupMQTT() {
   mqttClient.setServer(mqttServer, mqttPort);
   mqttClient.setCallback(callback);  
 }
 
-void reconnect() {
-  
+/* if connection is broken, try to reconnect  */
+void reconnect() {  
   while (!mqttClient.connected()) {
-      String clientId = "ESP32Client-";
-      clientId += String(random(0xffff), HEX);
       if (mqttClient.connect("capilano")) {
         Serial.println("connection");
         mqttClient.subscribe("capilano/basement/air");  // subscribe to topic
@@ -86,7 +79,7 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
-  String messageTemp;
+  String messageTemp = "";
   
   for (int i = 0; i < length; i++) {
     Serial.print((char)message[i]);
@@ -112,6 +105,14 @@ void callback(char* topic, byte* message, unsigned int length) {
 }
 
 
+/* Finds absolute humidity */
+uint32_t getAbsoluteHumidity(float temperature, float humidity) {
+  float absoluteHumidity = 213.7f * (humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature);
+  uint32_t scaledHumidity = (uint32_t) (1000.0f * absoluteHumidity); 
+  return scaledHumidity;
+}
+
+/* Connect to WIFI*/
 void connectToWiFi() {
   printTFT(1, 1, 4, "Connecting initialize..."); // This uses the standard ADAFruit small font
   WiFi.mode(WIFI_STA);
@@ -120,6 +121,7 @@ void connectToWiFi() {
     printTFT(1, 1, 4, "Wait ...."); // Overwrite the text to clear it
   }
 }
+/* ########################### FUNCTIONS END ################################# */ 
 
 void setup() {
   
@@ -127,9 +129,11 @@ void setup() {
 
   connectToWiFi();
 
-  printTFT(1,1,4, "Connected");
+  printTFT(1, 1, 4, "Connected");
 
   setupMQTT();
+
+  gas.calibrate();
 
   Wire.begin();
   Wire.setClock(400000);
@@ -153,9 +157,10 @@ void loop() {
   if (!mqttClient.connected()) {
     reconnect();
   }
+  Serial.println("connected ");
   mqttClient.loop();
   long now = millis();
-  if (now - last_time > 12000) {
+  if (now - last_time > 10000) {
     mySensor.setHumidity(8200);
     mySensor.measureAirQuality();
     tft.setCursor(0, 20, 4); // font 4
@@ -176,14 +181,17 @@ void loop() {
     tft.printf("%4d", mySensor.ethanol);
 
     tft.println();
-    int no2 = analogRead(NO2);
-    int nh3 = analogRead(NH3);
-    int co = analogRead(CO);
-    tft.print("NO2 : "); tft.print(no2);
-    tft.print(" NH3 : "); tft.print(nh3);
-    tft.print(" CO  : "); tft.print(co);
+    int no2 = gas.measure(NO2)*100;
+    delay(50);
+    int nh3 = gas.measure(NH3) * 100;
+    delay(50);
+    int co = gas.measure(CO) * 100;
+    delay(50);
+    tft.print("NO2 : "); tft.print(NO2);
+    tft.print(" NH3 : "); tft.print(NH3);
+    tft.print(" CO  : "); tft.print(CO);
 
-    sprintf(myData, "[{'myTime':'None'},{'CO2':%d},{'TVOC':%d},{'H2':%d},{'ETH':%d},{'NO2':%d},{'NH3':%d},{'CO':%d}]", mySensor.CO2, mySensor.TVOC, mySensor.H2, mySensor.ethanol, no2, nh3, co);
+    sprintf(myData, "[{'myTime':'None'},{'CO2*':%d},{'TVOC*':%d},{'H2*':%d},{'ETH*':%d},{'NO2':%d},{'NH3':%d},{'CO':%d}]", mySensor.CO2, mySensor.TVOC, mySensor.H2, mySensor.ethanol, no2, nh3, co);
     Serial.println(myData);
 
     mqttClient.publish("capilano/basement/air", myData);
